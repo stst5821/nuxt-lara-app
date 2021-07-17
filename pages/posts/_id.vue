cc9d4832cabfe68c946611dafe534455ab328138
stst5821 <stst5821@gmail.com>
2021-07-17 15:45:16 +0900

    id.vueにvalidation追加

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -49,6 +49,9 @@
         編集完了
       </button>
     </form>
+    <!-- バリデーション結果 -->
+    <p class="text-red-500 font-bold">{{ Validation.name_result }}</p>
+    <p class="text-red-500 font-bold">{{ Validation.body_result }}</p>
   </div>
 </template>
 
@@ -70,23 +73,56 @@ export default {
       id: '',
       name: '',
       body: '',
+      Validation: {
+        name_result: '',
+        body_result: '',
+      },
     }
   },
   methods: {
     // laravelのPostControllerのstoreにデータを送る。
     submit() {
-      // DBへの登録が完了したら、getでDBからデータを取ってきて追加したデータを画面に表示させる。
-      axios
-        .post(process.env.baseUrl + 'api/update', {
-          id: this.results.id,
-          name: this.results.name,
-          body: this.results.body,
-        })
-        .then(() => {
-          axios.get(process.env.baseUrl + `api/edit/68`).then((response) => {
-            this.result = response.data.result
+      let nameBool = false
+      let bodyBool = false
+
+      if (!this.results.name) {
+        this.Validation.name_result = '名前を入力してください'
+      } else {
+        nameBool = true
+        this.Validation.name_result = ''
+      }
+
+      if (!this.results.body) {
+        this.Validation.body_result = '本文を入力してください'
+      } else {
+        bodyBool = true
+        this.Validation.body_result = ''
+      }
+
+      if (nameBool === true && bodyBool === true) {
+        // DBへの登録が完了したら、getでDBからデータを取ってきて追加したデータを画面に表示させる。
+        axios
+          .post(process.env.baseUrl + 'api/update', {
+            id: this.results.id,
+            name: this.results.name,
+            body: this.results.body,
           })
-        })
+          .then(() => {
+            axios.get(process.env.baseUrl + `api/edit/68`).then((response) => {
+              this.result = response.data.result
+            })
+          })
+        this.Validation.result = '送信に成功しました！'
+        console.log(this.name)
+        this.name = ''
+        this.body = ''
+        this.Validation.name_result = ''
+        this.Validation.body_result = ''
+      }
+    },
+    checkString(inputdata) {
+      const regex = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+      return regex.test(inputdata)
     },
   },
 }
